a7108d0a3e9c2ad9a02219cf0be0b0379eb889ff
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-05-04 02:19:26 -0400

    Upgrade stuff
    update things to latest nh version, rename bodies, and fix typos. also make satellite and ship into their own assetbundle for collision.

--- ADreamOfHome/ADreamOfHome.cs
+++ ADreamOfHome/ADreamOfHome.cs
@@ -1,11 +1,19 @@
-﻿using OWML.Common;
+﻿using Epic.OnlineServices;
+using Epic.OnlineServices.Presence;
+using OWML.Common;
 using OWML.ModHelper;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ADreamOfHome;
 
 public class ADreamOfHome : ModBehaviour
 {
-	private void Awake()
+    private static INewHorizons newHorizons;
+
+
+    private void Awake()
 	{
 		// You won't be able to access OWML's mod helper in Awake.
 		// So you probably don't want to do anything here.
@@ -17,9 +25,10 @@ public class ADreamOfHome : ModBehaviour
 		// Starting here, you'll have access to OWML's mod helper.
 		ModHelper.Console.WriteLine($"My mod {nameof(ADreamOfHome)} is loaded!", MessageType.Success);
 
-		// Get the New Horizons API and load configs
-		var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+        // Get the New Horizons API and load configs
+        newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
 		newHorizons.LoadConfigs(this);
+		newHorizons.GetBodyLoadedEvent().AddListener(name => OnBodyLoaded(newHorizons.GetCurrentStarSystem(), name, newHorizons.GetPlanet(name)));
 
 		// Example of accessing game code.
 		LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
@@ -28,5 +37,59 @@ public class ADreamOfHome : ModBehaviour
 			ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
 		};
 	}
+
+    private void OnBodyLoaded(string starSystem, string name, GameObject body)
+    {
+        if (name == "Twisting Climate Hot Side" || name == "Twisting Climate Cold Side")
+        {
+            OnTwistedClimateSideLoaded(body, name.Contains("Hot"));
+        }
+        if (name == "Shrouded Tempest")
+        {
+            OnShroudedTempestLoaded(body);
+        }
+        if (name == "Molten Cherry")
+        {
+            OnMoltenCherryLoaded(body);
+        }
+    }
+
+    private void OnTwistedClimateSideLoaded(GameObject body, bool hot)
+    {
+        body.GetComponentInChildren<GravityVolume>().GetComponent<SphereCollider>().radius = 1280;
+        body.GetComponentInChildren<GravityVolume>()._lowerSurfaceRadius = 0;
+        body.GetComponentInChildren<GravityVolume>()._upperSurfaceRadius = 320;
+        var originalSector = newHorizons.GetPlanet("Twisted Climate").transform.Find("Sector");
+        var sector = body.transform.Find("Sector");
+        sector.GetComponent<SphereShape>().radius = 1290;
+        sector.GetComponent<Sector>().SetParentSector(originalSector.GetComponent<Sector>());
+        sector.gameObject.name = hot ? "Sector_HotSide" : "Sector_ColdSide";
+    }
+	
+    private void OnShroudedTempestLoaded(GameObject body)
+    {
+        body.transform.Find("Sector/Ring").GetComponent<MeshRenderer>().sharedMaterial.renderQueue = 4001;
+    }
+
+	private void OnMoltenCherryLoaded(GameObject body)
+	{
+        var meteorLauncher = body.GetComponentInChildren<MeteorLauncher>();
+        meteorLauncher.gameObject.SetActive(false);
+        var veryActiveLauncher = meteorLauncher.gameObject.AddComponent<VeryActiveMeteorLauncher>();
+        veryActiveLauncher._meteorPrefab = meteorLauncher._meteorPrefab;
+        veryActiveLauncher._meteorPrefab.GetComponentInChildren<DynamicForceDetector>()._activeInheritedDetector = body.GetComponentInChildren<ConstantForceDetector>();
+        veryActiveLauncher._dynamicMeteorPrefab = meteorLauncher._dynamicMeteorPrefab;
+        veryActiveLauncher._dynamicProbability = meteorLauncher._dynamicProbability;
+        veryActiveLauncher._audioSector = meteorLauncher._audioSector;
+        veryActiveLauncher._minLaunchSpeed = meteorLauncher._minLaunchSpeed;
+        veryActiveLauncher._maxLaunchSpeed = meteorLauncher._maxLaunchSpeed;
+        veryActiveLauncher._minInterval = meteorLauncher._minInterval;
+        veryActiveLauncher._maxInterval = meteorLauncher._maxInterval;
+        veryActiveLauncher._launchParticles = meteorLauncher._launchParticles;
+        veryActiveLauncher._launchSource = meteorLauncher._launchSource;
+        veryActiveLauncher._launchDirection = meteorLauncher._launchDirection;
+        GameObject.DestroyImmediate(meteorLauncher);
+        veryActiveLauncher.gameObject.SetActive(true);
+    }
 }
 
---
+++ ADreamOfHome/VeryActiveMeteorLauncher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ADreamOfHome
+{
+    public class VeryActiveMeteorLauncher : MeteorLauncher, ILateInitializer
+    {
+        public int meteorCount = 256;
+        public int dynamicMeteorCount => meteorCount/2;
+
+        private new void Awake()
+        {
+            _parentBody = gameObject.GetAttachedOWRigidbody();
+            _initialized = false;
+            LateInitializerManager.RegisterLateInitializer(this);
+        }
+
+        private new void OnDestroy()
+        {
+            if (!_initialized)
+            {
+                LateInitializerManager.UnregisterLateInitializer(this);
+            }
+        }
+
+        public new void LateInitialize()
+        {
+            _initialized = true;
+            if (_meteorPrefab != null)
+            {
+                _meteorPool = new List<MeteorController>(meteorCount);
+                _launchedMeteors = new List<MeteorController>(meteorCount);
+                for (int i = 0; i < meteorCount; i++)
+                {
+                    MeteorController requiredComponent = GameObject.Instantiate(_meteorPrefab).GetRequiredComponent<MeteorController>();
+                    requiredComponent.Suspend(base.transform);
+                    _meteorPool.Add(requiredComponent);
+                }
+            }
+            if (_dynamicMeteorPrefab != null)
+            {
+                _dynamicMeteorPool = new List<MeteorController>(dynamicMeteorCount);
+                _launchedDynamicMeteors = new List<MeteorController>(dynamicMeteorCount);
+                for (int j = 0; j < dynamicMeteorCount; j++)
+                {
+                    MeteorController requiredComponent2 = GameObject.Instantiate(_dynamicMeteorPrefab).GetRequiredComponent<MeteorController>();
+                    requiredComponent2.Suspend(base.transform);
+                    _dynamicMeteorPool.Add(requiredComponent2);
+                }
+            }
+        }
+    }
+}
